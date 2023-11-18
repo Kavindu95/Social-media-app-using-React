40d033a02e2fc63ff051dd6b26868de436178896
Kavindu95 <kavindueng95@gmail.com>
2023-11-18 12:27:12 +0530

    share-finished

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,10 @@
 import React from "./feed.css"
+import Share from "./Share"
 
 export default function Feed() {
   return (
     <div className="feed">
-      Feed
+      <Share />
     </div>
   )
 }
