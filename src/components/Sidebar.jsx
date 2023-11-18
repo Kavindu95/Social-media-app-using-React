1900cacb9aa5bf4860c5868cfbb839e267bd57fb
Kavindu95 <kavindueng95@gmail.com>
2023-11-18 11:25:23 +0530

    finished sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,62 @@
-import React from "./sidebar.css"
+import React from "./sidebar.css";
+import { MdRssFeed } from "react-icons/md";
+import { BsChatSquareQuoteFill } from "react-icons/bs";
+import { BiSolidVideos } from "react-icons/bi";
+import { MdGroups2 } from "react-icons/md";
+import { BsFillBookmarkStarFill } from "react-icons/bs";
+import { FaClipboardQuestion } from "react-icons/fa6";
+import { SiFramework } from "react-icons/si";
+import { MdEmojiEvents } from "react-icons/md";
+
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">
-      Sidebar
+    <div className="sidebar ">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+        <li className="sidebarListItem">
+          <MdRssFeed />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+          <BsChatSquareQuoteFill />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+          <BiSolidVideos />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+          <MdGroups2 />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+          <BsFillBookmarkStarFill />
+            <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+          <FaClipboardQuestion />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+          <SiFramework />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+          <MdEmojiEvents />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          {/* {Users.map((u) => (
+            <CloseFriend key={u.id} user={u} />
+          ))} */}
+        </ul>
+
+      </div>
     </div>
   )
 }
