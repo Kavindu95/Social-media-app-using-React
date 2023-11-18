a1cdf114c05db7fea0cbdca6da9976e458b14dff
Kavindu95 <kavindueng95@gmail.com>
2023-11-18 14:24:41 +0530

    set-posts

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,16 @@
 import React from "./feed.css"
 import Share from "./Share"
+import Post from "./Post"
 
 export default function Feed() {
   return (
     <div className="feed">
+      <div className="feedWrapper">
       <Share />
+      {/* <Post /> */}
+     
+      </div>
+      
     </div>
   )
 }
